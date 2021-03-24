98d6926bc2464bbd6eeb5c72dad3195b2f98443a
giresse.ashu <giresse.ashu@kuehne-nagel.com>
2021-03-24 08:40:36 +0200

    Initialized new Vue + ts app.

---
+++ Front-end/trading101/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Homepage msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Homepage from "./components/Homepage.vue";
+
+@Options({
+  components: {
+    Homepage,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
