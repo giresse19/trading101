04b694511652c8b8a9fcd3ef4364893cdcc090fd
giresse.ashu <giresse.ashu@kuehne-nagel.com>
2021-05-27 13:29:18 +0300

    Adjust AuthorizationFilter with jwt.

--- src/main/java/com/appsdeveloperblog/app/ws/mobileappws/web/utils/security/AuthorizationFilter.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/web/utils/security/AuthorizationFilter.java
@@ -18,26 +18,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthorizationFilter extends BasicAuthenticationFilter {
-
-//    private final UserRepository userRepository;
     private final UserServiceImpl userService;
     private final JwtUtils jwtUtils;
 
     public AuthorizationFilter(AuthenticationManager authenticationManager, UserServiceImpl userService, JwtUtils jwtUtils) {
         super(authenticationManager);
-//        this.userRepository = userRepository;
         this.userService = userService;
         this.jwtUtils = jwtUtils;
     }
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        String header = request.getHeader(SecurityConstants.HEADER_STRING);
-//        if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
-//            chain.doFilter(request, response);
-//            return;
-//        }
-
         try {
             String jwt = parseJwt(request);
             if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
@@ -54,9 +45,6 @@ public class AuthorizationFilter extends BasicAuthenticationFilter {
             logger.error("Cannot set user authentication: {}", e);
         }
 
-//        UsernamePasswordAuthenticationToken authenticationToken = getAuthentication(request);
-//        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
         chain.doFilter(request, response);
     }
 
@@ -69,32 +57,4 @@ public class AuthorizationFilter extends BasicAuthenticationFilter {
 
         return null;
     }
-
-    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
-
-        String token = request.getHeader(SecurityConstants.HEADER_STRING);
-
-        if (token != null) {
-
-            token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
-
-            String user = Jwts.parser()
-                    .setSigningKey(SecurityConstants.getTokenSecret())
-                    .parseClaimsJws(token)
-                    .getBody()
-                    .getSubject();
-
-//            if (user != null) {
-//                User userEntity = userRepository.findByEmail(user);
-//                if (userEntity == null) return null;
-//
-//                UserPrincipal userPrincipal = new UserPrincipal(userEntity);
-//                return new UsernamePasswordAuthenticationToken(userPrincipal, null, userPrincipal.getAuthorities());
-//            }
-
-            return null;
-        }
-        return null;
-    }
-
 }
